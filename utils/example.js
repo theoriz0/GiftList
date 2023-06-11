0cdb58f899b2292a9549f85b85b50dad513964c0
theoriz0 <zhang_00710@qq.com>
2023-06-11 12:01:10 +0800

    try name not in list / fake proof

--- utils/example.js
+++ utils/example.js
@@ -9,11 +9,26 @@ const merkleTree = new MerkleTree(niceList);
 const root = merkleTree.getRoot();
 
 // find the proof that norman block is in the list 
-const name = 'Norman Block';
-const index = niceList.findIndex(n => n === name);
-const proof = merkleTree.getProof(index);
+let name = 'Norman Block';
+let index = niceList.findIndex(n => n === name);
+let proof = merkleTree.getProof(index);
 
 // verify proof against the Merkle Root
 console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+// Name not in list
+name = 'H Zett M';
+index = niceList.findIndex(n => n === name);
+proof = merkleTree.getProof(index);
+
+// verify proof against the Merkle Root
+console.log( verifyProof(proof, name, root) );
+
+// Fake proof
+name = 'H Zett M';
+index = 0;
+proof = merkleTree.getProof(index);
+
+// verify proof against the Merkle Root
+console.log( verifyProof(proof, name, root) );
